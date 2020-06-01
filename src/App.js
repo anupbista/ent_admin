3e8df5e29a5baa59acde76809d6dcef96f39b0fc
anupbista <bistaanup77@gmail.com>
2020-06-01 21:02:55 +0545

    App optimized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { Router } from 'react-router-dom';
-import { Chart } from 'react-chartjs-2';
 import { ThemeProvider } from '@material-ui/styles';
 import validate from 'validate.js';
 
-import { chartjs } from './helpers';
 import theme from './theme';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import './assets/scss/index.scss';
@@ -15,10 +13,6 @@ import GlobalContextProvider from './contexts/GlobalContext';
 import SnackbarContextProvider from './contexts/SnackbarContext';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
-
-Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
-  draw: chartjs.draw
-});
 
 validate.validators = {
   ...validate.validators,
